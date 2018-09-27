3ee4de9173449453f1b821963dd4ebaa86112c24
Miguel Hernando <miguel.hernando@upm.es>
2018-09-27 20:48:10 +0200

    landmark...more

--- src/sim/sensors/landmark.cpp
+++ src/sim/sensors/landmark.cpp
@@ -5,97 +5,46 @@ namespace mr
 {
 	IMPLEMENT_MR_OBJECT(LandMark)
 
-	LandMark::LandMark()
+	LandMark::LandMark(int mark_id)
 	{
-		setDrawReferenceSystem(); //by default the refence system is drawn
-		setColor(1, 1, 0);
-		sensorActivated = true; //by default, when simulated... the values are computed
-		measure = 3;
-		setSensorProperties(DEG2RAD*10, 3, 0.05, 0);
-		beamNeedToBeUpdated = true;
-		//el cristal se pinta pero no se usa en la detección
-		PrismaticPart *piezo = new PrismaticPart();
-		piezo->setColor(0.9, 0.9, 0.9);
-		piezo->setRegularPolygonBase(0.025, 6);
-		piezo->setHeight(0.005);
-		piezo->setRelativeOrientation(0,M_PI/2,0);
-		//sensor non detectable by raytracing
-		piezo->setIntersectable(false);
-		(*this) += piezo;
+		this->mark_id = mark_id;
 	}
-
 
 	LandMark::~LandMark()
 	{
 	}
 
-
 	void LandMark::writeToStream(Stream& stream)
 	{
 		PositionableEntity::writeToStream(stream);
 		//data.writeToStream(stream);
-		stream << conusAngle << maxRange << minRange << gaussianError;
+		stream << mark_id;
 
 	}
 	void LandMark::readFromStream(Stream& stream)
 	{
 		PositionableEntity::readFromStream(stream);
 		//data.readFromStream(stream);
-		stream >> conusAngle >> maxRange >> minRange >> gaussianError;
-		setSensorProperties(conusAngle, maxRange, minRange, gaussianError);
+		stream >> mark_id;
 	}
 	void LandMark::writeToXML(XMLElement* parent)
 	{
-
-		//	XMLAux aux;
-		//	XMLElement* lasersensor=new XMLElement(parent,"laserSensorSim");
-		XMLVariable* _conusAngle = new XMLVariable("conusAngle", XMLAux::string_Convert<double>(conusAngle).c_str());
-		XMLVariable* _maxRange = new XMLVariable("maxRange", XMLAux::string_Convert<double>(maxRange).c_str());
-		XMLVariable* _minRange = new XMLVariable("minRange", XMLAux::string_Convert<double>(minRange).c_str());
-		XMLVariable* _error = new XMLVariable("error", XMLAux::string_Convert<double>(gaussianError).c_str());
-
-
-		parent->AddVariable(_conusAngle);
-		parent->AddVariable(_maxRange);
-		parent->AddVariable(_minRange);
-		parent->AddVariable(_error);
-
-
+		XMLVariable* _mark_id = new XMLVariable("mark_id", XMLAux::string_Convert<int>(mark_id).c_str());
+		parent->AddVariable(_mark_id);
 		PositionableEntity::writeToXML(parent);
 
 	}
 
 	void LandMark::readFromXML(XMLElement* parent)
 	{
-		double _conusAngle = conusAngle, _maxRange = maxRange, _minRange = minRange, _error = gaussianError;
-
-		if (parent->FindVariableZ("conusAngle"))
-		{
-			_conusAngle = XMLAux::GetValueDouble(parent->FindVariableZ("conusAngle"));
-		}
-		if (parent->FindVariableZ("maxRange"))
-		{
-			_maxRange = XMLAux::GetValueDouble(parent->FindVariableZ("maxRange"));
-		}
-
-		if (parent->FindVariableZ("minRange"))
-		{
-			_minRange = XMLAux::GetValueDouble(parent->FindVariableZ("minRange"));
-		}
-		if (parent->FindVariableZ("error"))
-		{
-			_error = XMLAux::GetValueDouble(parent->FindVariableZ("error"));
-		}
-
-		setSensorProperties(_conusAngle, _maxRange, _minRange, _error);
-
+		if (parent->FindVariableZ("mark_id"))
+			mark_id = parent->FindVariableZ("mark_id")->GetValueInt();
 		PositionableEntity::readFromXML(parent);
-
 	}
 
 	char* LandMark::CreateXMLText()
 	{
-		XMLElement* elem = new XMLElement(0, "UltrasonicSensor");
+		XMLElement* elem = new XMLElement(0, "LandMark");
 		writeToXML(elem);
 		return elem->CreateXMLText();
 	}
@@ -114,14 +63,9 @@ namespace mr
 
 
 	//set sensor properties
-	void LandMark::setSensorProperties(double _conusangle, double _maxrange, double _minrange, double _gaussian_error)
+	void LandMark::setMarkId(int mark)
 	{
-
-		conusAngle = _conusangle;
-		maxRange = _maxrange;
-		minRange = _minrange;
-		gaussianError = _gaussian_error;
-
+		mark_id = mark;
 	}
 
 	void LandMark::drawGL()
